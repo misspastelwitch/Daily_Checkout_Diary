18574cf0d7897eccf082feb81a4e250db6d3d9d4
Stella <stellaclough@gmail.com>
2025-04-04 15:15:31 +0200

    more server stuff

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 
 import express from 'express'
 
+import { Liquid } from 'liquidjs';
+
 const app = express()
+
+// Stel Liquid in als 'view engine'
+const engine = new Liquid();
+app.engine('liquid', engine.express()); 
+
+// Stel de map met Liquid templates in
+// Let op: de browser kan deze bestanden niet rechtstreeks laden (zoals voorheen met HTML bestanden)
+app.set('views', './views')
 
 app.use(express.static('public'))
 
 app.use(express.urlencoded({extended: true}))
+
+app.get('/', async function(request, response){
+response.render('index.liquid')
+})
 
 app.set('port', process.env.PORT || 8000)
 
